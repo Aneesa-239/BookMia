6eb2c6cb23a833555a5e4df4794d968c508648bf
SlowTyper-sys <82166973+SlowTyper-sys@users.noreply.github.com>
2022-12-22 14:33:24 +0200

    register cleanup

--- assets/php/register.php
+++ assets/php/register.php
@@ -1,65 +1,38 @@
 <?php
-<<<<<<< Updated upstream
 echo "register is called";
-=======
->>>>>>> Stashed changes
+
 //call the database config file, with the database connection
 require_once "config.php";
-$Name = $_POST['name'];
-$Surname = $_POST['surname'];
-$Email = $_POST['email'];
-$Phone = $_POST['phone'];
-$Password = $_POST['password'];
 
-<<<<<<< Updated upstream
-echo "$Name, $Surname,$Email', '$Phone, $Password";
-//database SQL insert code
-/*$sql = "INSERT INTO User (UserCode, FirstName, EmailAddress, PhoneNumber, Password) 
-        VALUES ('#US002', '$Name', $Surname','$Email', '$Phone', '$Password')";
-//Insert into the database
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+if (isset($_POST["submit"])) {
 
-$conn->close(); */
-$query = "insert into User(UserCode,FirstName,Lastname,EmailAddress,PhoneNumber,Password) values('#US002', '$Name', $Surname','$Email', '$Phone', '$Password')";
-$run = mysqli_query($conn, $query) or die(mysqli_error());
+    $Name = $_POST['name'];
+    $Surname = $_POST['surname'];
+    $Email = $_POST['email'];
+    $Phone = $_POST['phone'];
+    $Password = $_POST['password'];
 
-if ($run) {
-    echo "Form submitted";
-    header("location: index-2.html");
-    exit();
-} else {
-    echo "Form not submitted";
+    echo "$Name, $Surname,$Email', '$Phone, $Password";
+
+    $query = "insert into User(UserCode,FirstName,Lastname,EmailAddress,PhoneNumber,Password) 
+            values('#US002', '$Name', $Surname','$Email', '$Phone', '$Password')";
+    $run = mysqli_query($conn, $query) or die(mysqli_error());
+
+    if ($run) {
+        echo "Form submitted";
+        header("location: index-2.html");
+        exit();
+    } else {
+        echo "Form not submitted";
+    }
 }
 
 
-//if (mysqli_query($conn,$sql)){
-//Insert into the database
-// if ($conn->query($sql) === TRUE) 
-//echo "New record created successfully";
-//redirect the user to home page
 
 
 
-//} else {
-//echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 
 
 mysqli_close($conn);
 
-=======
-//database SQL insert code
-$sql = "INSERT INTO `User` (`UserCode`, `FirstName`, `EmailAddress`, `PhoneNumber`, `Password`) 
-        VALUES ('#US002', '$Name', $Surname','$Email', '$Phone', '$Password')";
-//Insert into the database
-$rs = mysqli_query($con, $sql);
-
-if ($rs) {
-    echo "Contact Records Inserted";
-}
-
->>>>>>> Stashed changes
 ?>
