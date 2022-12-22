2b2904a939e2865ebab0f4ccb595286598c42568
SlowTyper-sys <82166973+SlowTyper-sys@users.noreply.github.com>
2022-12-22 23:15:55 +0200

    Altered the register.php

--- assets/php/register.php
+++ assets/php/register.php
@@ -6,33 +6,22 @@ require_once "config.php";
 
 if (isset($_POST["submit"])) {
 
+
     $Name = $_POST['name'];
     $Surname = $_POST['surname'];
     $Email = $_POST['email'];
     $Phone = $_POST['phone'];
     $Password = $_POST['password'];
 
-    echo "$Name, $Surname,$Email', '$Phone, $Password";
 
-    $query = "insert into User(UserCode,FirstName,Lastname,EmailAddress,PhoneNumber,Password) 
+
+    $query = "insert into User(UserCode,FirstName,Lastname,EmailAddress,PhoneNumber,UserPassword) 
             values('#US002', '$Name', $Surname','$Email', '$Phone', '$Password')";
-    $run = mysqli_query($conn, $query) or die(mysqli_error());
 
-    if ($run) {
-        echo "Form submitted";
-        header("location: index-2.html");
-        exit();
-    } else {
-        echo "Form not submitted";
-    }
+    echo "$Name, $Surname,$Email', '$Phone, $Password";
+    mysqli_query($conn, $query);
+
+    mysqli_close($conn);
+    header("Location:index-2.html");
 }
-
-
-
-
-
-
-
-mysqli_close($conn);
-
 ?>
