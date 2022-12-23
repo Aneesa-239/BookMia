6203609cef0fe932fc56e69a7a8045c8a20a13f7
SlowTyper-sys <82166973+SlowTyper-sys@users.noreply.github.com>
2022-12-23 11:19:42 +0200

    added patient table bookmark

--- assets/php/register.php
+++ assets/php/register.php
@@ -1,4 +1,10 @@
 <?php
+
+/*
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+*/
+
 echo "register is called";
 
 //call the database config file, with the database connection
@@ -6,20 +12,38 @@ require_once "config.php";
 
 if (isset($_POST["submit"])) {
 
-
     $Name = $_POST['name'];
     $Surname = $_POST['surname'];
     $Email = $_POST['email'];
     $Phone = $_POST['phone'];
     $Password = $_POST['password'];
 
-    $query = "insert into User(FirstName,Lastname,EmailAddress,PhoneNumber,UserPassword) 
-            values('$Name', $Surname','$Email', '$Phone', '$Password')";
+    $query = "INSERT INTO User ( FirstName, LastName, EmailAddress, PhoneNumber, UserPassword, IsAdmin) 
+            VALUES ('$Name', '$Surname', '$Email', '$Phone', '$Password', '0')";
 
-    echo "$Name, $Surname,$Email', '$Phone, $Password";
+    //when Patient logs in they will have to be saved into the Patient Table.
+
+    echo "$Name, $Surname, $Email, $Phone, $Password";
+
     mysqli_query($conn, $query);
 
     mysqli_close($conn);
-    header("Location:index-2.html");
+
+    header("Location: index-2.html");
+
+    /*
+    $run = mysqli_query($conn, $query) or die(mysqli_error());
+    if ($run) {
+    echo "Form submitted";
+    header("location: index-2.html");
+    exit();
+    } 
+    else {
+    echo "Form not submitted";
+    }
+    mysqli_close($conn);
+    */
+
 }
+
 ?>
