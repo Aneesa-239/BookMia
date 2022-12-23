0cf8adfdc735775331f2e9779b4b2825b5e3a37e
Aneesa-239 <aneesam2000@gmail.com>
2022-12-23 11:21:23 +0200

    Login.php is up and running please not that i have not implemented sessions and thuis user profile will not work right. login.html

--- assets/php/login.php
+++ assets/php/login.php
@@ -1,44 +1,29 @@
 <?php
+echo "login has been called";
 
+//call the database config file, with the database connection
 require_once "config.php";
-require_once "session.php";
 
-$error = '';
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
+if(isset($_POST['submit'])){
 
-    $email = trim ($_POST['p_email']); // giving the name you called input in html another name
-    $password = trim($_POST['p_password']);
+$Email = $_POST["p_email"];
+$password = $_POST["p_password"];
 
-    //validate if email is empty
-    if (empty($email)) {
-        $error .= '<p class="error"> Please enter email.</p>';
-    }
 
-    //validate if password is empty
-    if (empty($password)) {
-        $error .= '<p class="error"> Please enter password.</p>';
-    }
-
-    if (empty($error)) {
-        if ($query = $db -> prepare("SELECT * FROM user WHERE email = ?")){
-            $query -> bind_param('s', $email);
-            $query -> execute();
-            $row = $query -> fetch();
-            if ($row){
-                if (password_verify($password, $row['password'])){
-                    $_SESSION["userid"] = $row['id'];
-                    $_SESSION["user"] = $row;
-
-                    //redirect the user to home page
-                    header("location: index-2.html");
-                    exit;
-                } else {
-                    $error .= '<p class = "error"> No user exist with that email address .</p>';
-                }
-            }
-            $query -> close();
-        }
-    //Close connection
-    mysqi_close($db); // always remember to close the db
+    echo "$Email, $password";
+    
 }
+
+$query = "SELECT * FROM User WHERE EmailAddress = '$Email' AND UserPassword = '$password'";
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    header("Location: index-2.html");
+} else {
+    echo "0 results";
+}
+
+   $conn->close();
+  
 ?>
