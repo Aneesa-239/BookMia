8ec437dc95a2045d82e98f5e3c253a32bc36418f
Aneesa-239 <aneesam2000@gmail.com>
2023-01-25 15:28:38 +0200

    Final Project upload from Server

---
+++ assets/php/booking-success2.php
@@ -0,0 +1,211 @@
+<?php 
+    session_start(); 
+
+	require_once('assets/php/config.php');
+	
+
+	
+		ini_set('display_errors', '1');
+		ini_set('display_startup_errors', '1');
+		error_reporting(E_ALL);
+		
+		$authsess = $_SESSION['name'];
+		
+?>
+
+
+
+<!DOCTYPE html> 
+<html lang="en">
+
+<head>
+		<meta charset="utf-8">
+		<title>BookMia</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+		
+		<!-- Favicons -->
+		<link href="assets/img/favicon.png" rel="icon">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+		
+		<!-- Main CSS -->
+		<link rel="stylesheet" href="assets/css/style.css">
+		
+	
+	
+	</head>
+	<body>
+
+		<!-- Main Wrapper -->
+		<div class="main-wrapper">
+		
+			<!-- Header -->
+			<header class="header">
+				<nav class="navbar navbar-expand-lg header-nav">
+					<div class="navbar-header">
+						<a id="mobile_btn" href="javascript:void(0);">
+							<span class="bar-icon">
+								<span></span>
+								<span></span>
+								<span></span>
+							</span>
+						</a>
+						<a href="index-2.php" class="navbar-brand logo">
+							<img src="assets/img/favicon.png" class="img-fluid" alt="Logo">
+						</a>
+					</div>
+					<div class="main-menu-wrapper">
+						<div class="menu-header">
+							<a href="index-2.php" class="menu-logo">
+								<img src="assets/img/favicon.png" class="img-fluid" alt="Logo">
+							</a>
+							<a id="menu_close" class="menu-close" href="javascript:void(0);">
+								<i class="fas fa-times"></i>
+							</a>
+						</div>
+						<ul class="main-nav">
+							<li>
+								<a href="index-2.php" style="color: #fefefe">Home</a>
+							</li>
+							<li>
+								<a href="search.php" style="color: #fefefe">Search Doctor</a>
+							</li>
+							<li>
+								<a href="p_doctor-profile.php" style="color: #fefefe" >Doctor Profile</a>
+							</li>
+							<li>
+								<a href="patient-dashboard.php"style="color: #fefefe" >Patient Dashboard</a>
+							</li>
+							
+							
+						
+						
+						</ul>	 
+						</div>		 
+					<<ul class="nav header-navbar-rht">
+						<li class="nav-item contact-item">
+	<div class="header-contact-detail">
+								<p class="contact-header">Welcome</p>
+								<p class="contact-info-header"> <?php echo $_SESSION['name'] . '!'?></p>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link header-login" href="assets/php/logout">logout</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<!-- /Header -->
+			
+			<!-- Breadcrumb -->
+			<div class="breadcrumb-bar">
+				<div class="container-fluid">
+					<div class="row align-items-center">
+						<div class="col-md-12 col-12">
+							<nav aria-label="breadcrumb" class="page-breadcrumb">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index-2.php">Home</a></li>
+									<li class="breadcrumb-item active" aria-current="page">Booking</li>
+								</ol>
+							</nav>
+							<h2 class="breadcrumb-title">Booking</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /Breadcrumb -->
+			
+			<!-- Page Content -->
+			<div class="content success-page-cont">
+				<div class="container-fluid">
+				
+					<div class="row justify-content-center">
+						<div class="col-lg-6">
+						
+							<!-- Success Card  $_GET['id'] = "BC001";-->
+							<div class="card success-card">
+								<div class="card-body">
+								    <?php
+								    
+							    
+								    
+	 if(isset($_GET['id']) && !empty($_GET['id'])){
+        
+        $app= $_GET['id'];
+
+	  $query = "SELECT * FROM Booking INNER JOIN Doctor ON Doctor.DoctorCode = Booking.DoctorCode INNER JOIN User ON User.UserCode = Doctor.UserCode WHERE Booking.BookingCode = '$app'";
+
+							
+	$result = mysqli_query($conn, $query);
+	$row = [];
+	
+	
+	
+	if ($result->num_rows > 0) {
+									// fetch all data from db into array 
+									$row = $result->fetch_all(MYSQLI_ASSOC);
+
+								}        
+									
+						
+                                foreach ($row as $rows) {
+                                   
+								    
+							?>				
+									<div class="success-cont">
+										<i class="fas fa-check"></i>
+										<h3>Booking was Successful!</h3>
+										<p>Appointment booked with <strong>Dr. <?php 
+													echo $rows['FirstName'];
+												?> 	
+													<?php echo $rows['LastName']?>
+													</strong><br> on <strong><?php 
+												        $time = new DateTime($rows["StartDate"]);
+												        $date = $time->format('d-M-Y');
+														echo $date;?> <?php 
+												        $time = new DateTime($rows["StartDate"]);
+												        $start= $time->format('H:m');
+														echo $start;?> - <?php 
+												        $time = new DateTime($rows["EndDate"]);
+												        $end = $time->format('H:m');
+														echo $end;?></strong></p>
+												
+														
+										<a href="https://divblox-pdf.azurewebsites.net/?api_key=7Qq9cT2PAgWzSmi4FI0R&pdf_url=http://www.motseki.net.za/bookmia/user/assets/php/invoice-view.php?id=<?php echo $app; ?>" name="submit" type="submit" class="btn btn-primary view-inv-btn">View Invoice</a>
+									
+									</div>
+									<?php } } ?>
+								</div>
+							</div>
+							<!-- /Success Card invoice-view.php?id=<?=$inv;?>-->
+							
+						</div>
+					</div>
+					
+				</div>
+			</div>		
+			<!-- /Page Content -->
+   
+		
+		   
+		</div>
+		<!-- /Main Wrapper -->
+	  
+		<!-- jQuery -->
+		<script src="assets/js/jquery.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+		
+	</body>
+
+</html>
---
+++ invoice-view2.php
@@ -0,0 +1,269 @@
+<?php
+
+    
+
+require_once('config.php');
+
+
+
+  
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+    
+    
+
+
+//if(isset($_GET['id']) && !empty($_GET['id'])) {
+
+  
+        
+      $app = $_GET['id'];
+      
+      $date = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+
+      $book = "SELECT * FROM Booking  WHERE BookingCode = '$app'";
+
+      $res1 = mysqli_query($conn, $book);
+      $bk = mysqli_fetch_assoc($res1);
+
+      $due = $bk['StartDate'];
+  
+      $doc = "SELECT * FROM User INNER JOIN Doctor ON Doctor.UserCode = User.UserCode INNER JOIN Booking ON Booking.DoctorCode = Doctor.DoctorCode WHERE Booking.BookingCode = '$app'";
+
+      $res = mysqli_query($conn, $doc);
+      $info = mysqli_fetch_assoc($res);
+
+      $d_name = $info['FirstName'];
+      $d_lastname = $info['LastName']; 
+      $d_loca = $info['Location'];
+
+      
+      $pat = "SELECT * FROM User INNER JOIN Patient ON Patient.UserCode = User.UserCode INNER JOIN Booking ON Booking.PatientCode = Patient.PatientCode WHERE Booking.BookingCode = '$app'";
+
+         
+      $res = mysqli_query($conn, $pat);
+      $data = mysqli_fetch_assoc($res);
+
+    
+      $name = $data['FirstName'];
+      $lastname = $data['LastName'];
+      $patemail = $data['address'];
+      $city = $data['city'];
+      $pro = $data['province'];
+      $zc = $data['zipcode'];
+      $count = $data['country'];
+      
+       
+      $price = "SELECT * FROM User INNER JOIN Doctor ON Doctor.UserCode = User.UserCode INNER JOIN Booking ON Booking.DoctorCode = Doctor.DoctorCode WHERE Booking.BookingCode = '$app'";
+
+         
+      $res = mysqli_query($conn, $price);
+      $data = mysqli_fetch_assoc($res);
+
+    
+      $amount = $data['Fees'];
+
+    
+
+
+  
+
+
+echo("
+
+
+<!DOCTYPE html> 
+<html lang='en'>
+  
+<head>
+    <meta charset='utf-8'>
+    <title>BookMia</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    
+    <!-- Favicons -->
+    <link href='../img/favicon.png' rel='icon'>
+    
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='../css/bootstrap.min.css'>
+    
+    <!-- Fontawesome CSS -->
+    <link rel='stylesheet' href='../plugins/fontawesome/css/fontawesome.min.css'>
+    <link rel='stylesheet' href='../plugins/fontawesome/css/all.min.css'>
+    
+    <!-- Main CSS -->
+    <link rel='stylesheet' href='../css/style.css'>
+  
+  </head>
+  <body>
+      
+      <!-- Page Content -->
+      <div class='content'>
+        <div class='container-fluid'>
+
+
+<div class='row'>
+            <div class='col-lg-8 offset-lg-2'>
+              <div class='invoice-content'>
+                  
+    
+                <div class='invoice-item'>
+                  <div class='row'>
+                    <div class='col-md-6'>
+                      <div class='invoice-logo'>
+                        <img src='../img/favicon.png' alt='logo'>
+                      </div>
+                    </div>
+                    <div class='col-md-6'>
+                      <p class='invoice-details'>
+                        <strong>Issued:</strong> 10/01/2023
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                
+                <!-- Invoice Item -->
+                <div class='invoice-item'>
+                  <div class='row'>
+                    <div class='col-md-6'>
+                      <div class='invoice-info'>
+          
+                        <strong class='customer-text'>Invoice From</strong>
+                        <p class='Invoice-details invoice-details-two'>
+                          Dr."	.$d_name."
+		" . $d_lastname . "<br> " . $d_loca . "
+                        <br>
+                          
+                        </p>
+                      </div>
+                    
+                      
+                    </div>
+                    <div class='col-md-6'>
+                      <div class='invoice-info invoice-info2'>
+        
+                        <strong class='customer-text'>Invoice To</strong>
+                        <p class='invoice-details'>
+                        " . $name . " " . $lastname . "   <br>
+                          " . $patemail . ", " . $city . "<br>
+                          " . $pro . ", " . $zc . ", " . $count . " <br>
+                        </p>
+                      </div>
+                
+                    </div>
+                  </div>
+                </div>
+                <!-- /Invoice Item -->
+                
+                <!-- Invoice Item -->
+                <div class='invoice-item'>
+                  <div class='row'>
+
+
+<div class='col-md-12'>
+                      <div class='invoice-info'>
+                        <strong class='customer-text'>Payment Due</strong>
+                        <p class='invoice-details invoice-details-two'>
+                          Credit Card <br>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /Invoice Item -->
+                
+                <!-- Invoice Item -->
+                <div class='invoice-item invoice-table-wrap'>
+                  <div class='row'>
+  
+                    <div class='col-md-12'>
+                      <div class='table-responsive'>
+                        <table class='invoice-table table table-bordered'>
+                          <thead>
+                            <tr>
+                              <th>Description</th>
+                              <th class='text-center'>Quantity</th>
+                              <th class='text-center'>VAT</th>
+                              <th class='text-right'>Total</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>General Consultation</td>
+                              <td class='text-center'>1</td>
+                              <td class='text-center'>15%</td>
+                              <td class='text-right'>R " . $amount . "</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                    <div class='col-md-6 col-xl-4 ml-auto'>
+                      <div class='table-responsive'>
+                        <table class='invoice-table-two table'>
+                          <tbody>
+                          <tr>
+                            <th>Subtotal:</th>
+                            <td><span>R " . $amount . "</span></td>
+                          </tr>
+                          
+                          <tr>
+                            <th>Total Amount:</th>
+                            <td><span>R " . $amount . "</span></td>
+                          </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                
+                  </div>
+                </div>
+                <!-- /Invoice Item -->
+                
+                <!-- Invoice Information -->
+                <div class='other-info'>
+                  <h4>Other information</h4>
+                  <p class='text-muted mb-0'>Welcome to BookMia® Medical Professional website. Mia Medical, LLC (“Mia Medical”) provides its Services to you (the “Buyer”) subject to the notices, terms and conditions of this agreement ('Agreement'). We reserve the right to change this website and these terms and conditions at any time.All new customers will be required to prepay. Unless otherwise indicated on the sales order, payment may be made via Credit Card, Check or Wire Transfer with the following considerations:Checks can be made payable to “Mia Medical” and sent to:Mia Medical, PO Box 775245, Gauteng ,South Africa.Credit card payments can be taken at http://www.motseki.net.za/bookmia/user/ or a payment link from the emailed invoice. Each payment MUST reference the Mia Medical Invoice Number or payment may be rejected.If payment is made via wire transfers: Buyer is responsible for all fees charged by Buyer’s banking or financial institution and any intermediary bank.</p>
+                </div>
+                <!-- /Invoice Information -->
+                
+              </div>
+              
+            </div>
+          </div>
+
+        </div>
+
+
+</div>    
+      <!-- /Page Content -->
+       
+     </div>
+     <!-- /Main Wrapper -->
+    
+    <!-- jQuery -->
+    <script src='assets/js/jquery.min.js'></script>
+    
+    <!-- Bootstrap Core JS -->
+    <script src='assets/js/popper.min.js'></script>
+    <script src='assets/js/bootstrap.min.js'></script>
+    
+    <!-- Slick JS -->
+    <script src='assets/js/slick.js'></script>
+    
+    <!-- Custom JS -->
+    <script src='assets/js/script.js'></script>
+    
+  </body>
+
+</html>
+");
+
+
+
+
+
+
+
+
+?>
